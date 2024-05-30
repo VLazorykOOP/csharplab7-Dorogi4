a8fb788c620a5fd861705029891180990236929b
Dorozhkin Oleksandr <dorozskin@gmail.com>
2024-05-30 13:28:25 +0300

    1 done

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Lab7CSharp
+﻿namespace Lab7CSharp
 {
     partial class Form1
     {
@@ -30,6 +29,8 @@ namespace Lab7CSharp
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -46,13 +47,31 @@ namespace Lab7CSharp
             this.label1.Text = "Lab 7.   C# ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(510, 93);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(461, 253);
+            this.panel1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 386);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -63,6 +82,7 @@ namespace Lab7CSharp
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
-
--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,25 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab7CSharp
 {
     public partial class Form1 : Form
     {
+        private Timer timer;
+        private double angle;
+        private Random random;
+        private Pen pen;
+
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Ініціалізація таймера
+            timer = new Timer();
+            timer.Interval = 50; // Інтервал таймера у мілісекундах
+            timer.Tick += Timer_Tick;
+
+            // Ініціалізація випадкового генератора
+            random = new Random();
+
+            // Ініціалізація пера для малювання
+            pen = new Pen(Color.Black, 2);
+
+            // Початковий кут
+            angle = 0;
+
+            // Запуск таймера
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Збільшення кута для обертання
+            angle += 0.1;
+
+            // Випадкова зміна кольору та ширини пера
+            pen.Color = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+            pen.Width = random.Next(1, 10);
+
+            // Обчислення нової позиції для Panel
+            int radius = 100; // Радіус обертання
+            int centerX = button1.Left + button1.Width / 2;
+            int centerY = button1.Top + button1.Height / 2;
+
+            int newX = centerX + (int)(radius * Math.Cos(angle)) - panel1.Width / 2;
+            int newY = centerY + (int)(radius * Math.Sin(angle)) - panel1.Height / 2;
+
+            panel1.Left = newX;
+            panel1.Top = newY;
+
+            // Виклик перерисування форми
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Малювання кола обертання
+            int radius = 100;
+            int centerX = button1.Left + button1.Width / 2;
+            int centerY = button1.Top + button1.Height / 2;
+            e.Graphics.DrawEllipse(pen, centerX - radius, centerY - radius, radius * 2, radius * 2);
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            // Додайте тут код для обробки події Click для label1
         }
     }
 }
